6f228b0ffdbd245302bcb62c1951001ab4d34ce4
Dustman <dreed12@mail.pima.edu>
2018-11-14 18:29:05 -0700

    player Prefab made
    Movement set u just need to do interaction and telekinesis script

---
+++ Assets/Scripts/DelegatesEvents/InputListener.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+public class InputListener : MonoBehaviour {
+
+    public static InputListener Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(this);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(this);
+    }
+
+    [HideInInspector]
+    public UnityFloatEvent EventForward;
+
+    [HideInInspector]
+    public UnityFloatEvent EventRight;
+
+    [HideInInspector]
+    public UnityEvent EventJump;
+
+    [HideInInspector]
+    public UnityEvent EventEscape;
+
+    [HideInInspector]
+    public UnityEvent EventInteract;
+
+    [HideInInspector]
+    public UnityFloatEvent EventMouseX;
+
+    [HideInInspector]
+    public UnityFloatEvent EventMouseY;
+
+    private bool _jumpPressed;
+    private bool _escapePressed;
+    private bool _interactPressed;
+
+    private void Update()
+    {
+        BroadcastForward();
+        BroadcastRight();
+        BroadcastJump();
+
+
+        BroadcastEscape();
+        BroadcastInteract();
+
+        BroadcastMouseX();
+        BroadcastMouseY();
+    }
+
+    private void BroadcastInteract()
+    {
+        if (EventInteract == null) return;
+
+        if (GetAxisDown("Interact", ref _interactPressed))
+        {
+            EventInteract.Invoke();
+        }
+    }
+
+    private void BroadcastMouseY()
+    {
+        if (EventMouseY == null) return;
+
+        var y = Input.GetAxis("Mouse Y");
+        EventMouseY.Invoke(y);
+    }
+
+    private void BroadcastMouseX()
+    {
+        if (EventMouseX == null) return;
+
+        var x = Input.GetAxis("Mouse X");
+        EventMouseX.Invoke(x);
+    }
+
+    private void BroadcastEscape()
+    {
+        if (EventEscape == null) return;
+
+        if (GetAxisDown("Cancel", ref _escapePressed))
+        {
+            EventEscape.Invoke();
+        }
+    }
+
+    private void BroadcastForward()
+    {
+        if (EventForward == null) return;
+
+        var fwd = Input.GetAxis("Vertical");
+        EventForward.Invoke(fwd);
+    }
+
+    private void BroadcastJump()
+    {
+        if (EventJump == null) return;
+
+        if (GetAxisDown("Jump", ref _jumpPressed))
+        {
+            EventJump.Invoke();
+        }
+    }
+
+    private void BroadcastRight()
+    {
+        if (EventRight == null) return;
+
+        var right = Input.GetAxis("Horizontal");
+        EventRight.Invoke(right);
+    }
+
+    private static bool GetAxisDown(string axis, ref bool isPressed)
+    {
+        if (!isPressed && Input.GetAxis(axis) > 0)
+        {
+            isPressed = true;
+            return true;
+        }
+
+        if (Mathf.Abs(Input.GetAxis(axis)) < 1)
+        {
+            isPressed = false;
+        }
+
+        return false;
+    }
+}
+/*
+ * 
+ * Much thanks to Andrew this is a good input controller
+ * aswell as the layer movement script
+ * 
+ */
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,200 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    public Vector3 Velocity;
+    public Vector3 WishDirection;
+
+    public float Dot;
+
+    public float Gravity = 20.0f;
+    public float JumpSpeed = 6.0f;
+    public float MoveSpeed = 4.0f;
+
+    public float GroundAcceleration = 15.0f;
+    public float GroundDeceleration = 35.0f;
+
+    public float AirAcceleration = 4.0f;
+    public float AirDeceleration = 5.0f;
+
+    public float CurrentSpeed;
+
+    public bool Decelerating;
+
+    public bool IsControlled;
+
+    private struct Movement
+    {
+        public float Forward;
+        public float Right;
+        public bool Jump;
+    }
+
+    private struct MouseInput
+    {
+        public float Vertical;
+        public float Horizontal;
+    }
+
+    private Movement _movement;
+    private MouseInput _mouseInput;
+    private CharacterController _controller;
+
+    private Transform _playerCam;
+
+    private void Start()
+    {
+        var cam = GetComponentInChildren<Camera>();
+        if (cam == null)
+        {
+            Debug.LogError("No Camera found as child of object with PlayerMovement!");
+        }
+        else
+        {
+            _playerCam = cam.transform;
+        }
+
+        _controller = GetComponent<CharacterController>();
+        Possess();
+    }
+
+    private void Update()
+    {
+        if (IsControlled)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        var wishDir = new Vector3(_movement.Right, 0, _movement.Forward).normalized;
+        wishDir = transform.TransformDirection(wishDir);
+        var lastVel = Velocity;
+        lastVel.y = 0;
+        var curDir = lastVel.normalized;
+
+        var moveDot = Vector3.Dot(curDir, wishDir);
+        Dot = moveDot;
+        var shouldAccelerate = (moveDot >= 0.01 || lastVel.sqrMagnitude <= 0.001f && wishDir.sqrMagnitude > 0);
+        float vel;
+        WishDirection = wishDir;
+
+        if (_controller.isGrounded)
+        {
+            vel = shouldAccelerate ? GroundAcceleration : GroundDeceleration;
+        }
+        else
+        {
+            vel = shouldAccelerate ? AirAcceleration : AirDeceleration;
+        }
+
+        if (shouldAccelerate)
+        {
+            wishDir *= vel * Time.deltaTime;
+            lastVel += wishDir;
+            Decelerating = false;
+        }
+        else
+        {
+            Decelerating = true;
+            lastVel -= (curDir * vel) * Time.deltaTime;
+        }
+
+        lastVel = Vector3.ClampMagnitude(lastVel, MoveSpeed);
+
+        if (lastVel.sqrMagnitude < 0.5 && !shouldAccelerate)
+            lastVel = Vector3.ClampMagnitude(lastVel, 0);
+
+        CurrentSpeed = Mathf.Abs(lastVel.magnitude);
+        lastVel.y = Velocity.y;
+
+        Velocity = lastVel;
+
+
+
+        if (_movement.Jump)
+        {
+            Velocity.y = JumpSpeed;
+            _movement.Jump = false;
+        }
+
+        else if (!_controller.isGrounded)
+            Velocity.y -= Gravity * Time.deltaTime;
+
+
+
+        _controller.Move(Velocity * Time.deltaTime);
+
+        var rot = transform.eulerAngles;
+
+        rot.y += _mouseInput.Horizontal;
+
+        transform.eulerAngles = rot;
+    }
+
+    private void LateUpdate()
+    {
+        var camRot = _playerCam.transform.localEulerAngles;
+
+        camRot.x += _mouseInput.Vertical;
+
+        _playerCam.transform.localEulerAngles = camRot;
+    }
+
+
+    private void Vertical(float val)
+    {
+        if (Application.isFocused)
+            _mouseInput.Vertical = val;
+        else
+            _mouseInput.Vertical = 0;
+    }
+
+    private void Horizontal(float val)
+    {
+        if (Application.isFocused)
+            _mouseInput.Horizontal = val;
+        else
+            _mouseInput.Horizontal = 0;
+    }
+
+    private void Jump()
+    {
+        if (_controller.isGrounded)
+        {
+            _movement.Jump = true;
+        }
+    }
+
+    private void MoveRight(float val)
+    {
+        _movement.Right = val;
+    }
+
+    private void MoveForward(float val)
+    {
+        _movement.Forward = val;
+    }
+
+    public void Possess()
+    {
+        var handle = InputListener.Instance;
+
+        handle.EventForward.AddListener(MoveForward);
+        handle.EventRight.AddListener(MoveRight);
+        handle.EventJump.AddListener(Jump);
+        handle.EventMouseX.AddListener(Horizontal);
+        handle.EventMouseY.AddListener(Vertical);
+        IsControlled = true;
+    }
+    public void Unpossess()
+    {
+        var handle = InputListener.Instance;
+        handle.EventForward.RemoveListener(MoveForward);
+        handle.EventRight.RemoveListener(MoveRight);
+        handle.EventJump.RemoveListener(Jump);
+        handle.EventMouseX.RemoveListener(Horizontal);
+        handle.EventMouseY.RemoveListener(Vertical);
+        IsControlled = false;
+    }
+}
