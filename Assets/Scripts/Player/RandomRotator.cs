91b2c462ea9942cb3ebbf0a7fb7b3549b2990267
Dustman <dreed12@mail.pima.edu>
2018-12-03 15:11:19 -0700

    more polish work
    camra has colider to make 3 blocks required to get by

---
+++ Assets/Scripts/Player/RandomRotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+    private Vector3 RandomRotation;
+
+    private void Start()
+    {
+        RandomRotation = new Vector3(Random.Range(8, 100), Random.Range(8, 100), Random.Range(8, 100));
+    }
+    void Update ()
+    {
+        transform.Rotate(RandomRotation, 1000 * Time.deltaTime);	
+	}
+}
